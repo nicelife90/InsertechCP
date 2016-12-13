cfb8e43beed69fee261015a0a9dcb675f0314fe3
Cédrick El'Yamani <celyamani@insertech.ca>
2016-12-13 16:19:22 -0500

    Dump sql

--- structure.sql
+++ structure.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.5.1
--- http://www.phpmyadmin.net
+-- version 4.6.4
+-- https://www.phpmyadmin.net/
 --
--- Client :  127.0.0.1
--- Généré le :  Sam 10 Décembre 2016 à 02:30
--- Version du serveur :  5.7.11
--- Version de PHP :  5.6.19
+-- Host: 127.0.0.1
+-- Generation Time: Dec 13, 2016 at 09:18 PM
+-- Server version: 5.7.14
+-- PHP Version: 5.6.25
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -17,13 +17,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de données :  `ced`
+-- Database: `ced`
 --
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `screen`
+-- Table structure for table `screen`
 --
 
 CREATE TABLE `screen` (
@@ -38,20 +38,23 @@ CREATE TABLE `screen` (
   `connector` tinytext COLLATE utf8_unicode_ci,
   `connector_position` tinytext COLLATE utf8_unicode_ci,
   `grade` varchar(5) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `image` text COLLATE utf8_unicode_ci,
   `user` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
--- Contenu de la table `screen`
+-- Dumping data for table `screen`
 --
 
-INSERT INTO `screen` (`id`, `date`, `model`, `size`, `resolution`, `revision`, `finition`, `technologie`, `connector`, `connector_position`, `grade`, `user`) VALUES
-(1, '2016-12-10 02:25:46', 'LP133WX2', '13.3', '1280 x 800', 'TLA1', 'Mat', 'LED', '40', 'Bas Droite', 'A', NULL);
+INSERT INTO `screen` (`id`, `date`, `model`, `size`, `resolution`, `revision`, `finition`, `technologie`, `connector`, `connector_position`, `grade`, `image`, `user`) VALUES
+(1, '2016-12-10 02:25:46', 'LP133WX2', '13.3', '1280 x 800', 'TLA1', 'Mat', 'LED', '40', 'Bas Droite', 'A', NULL, NULL),
+(2, '2016-12-13 19:12:09', 'FWEFWE', '12.1', '1152 x 864', 'FWEFEW', 'Brillant', 'LED', 'fwefewfew', 'Haut Gauche', 'A', 'uploads/585048090f13a_img0185.jpg', NULL),
+(3, '2016-12-13 19:51:46', 'FWEFWE', '12.1', '1280 x 768', 'FWEFEW', 'Brillant', 'LCD', 'fwefewfew', 'Haut Droite', 'A', 'uploads/58505152c89fa_img0178.jpg', NULL);
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `users`
+-- Table structure for table `users`
 --
 
 CREATE TABLE `users` (
@@ -66,40 +69,40 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
 
 --
--- Contenu de la table `users`
+-- Dumping data for table `users`
 --
 
 INSERT INTO `users` (`id`, `first_name`, `last_name`, `username`, `pwd_hash`, `email`, `last_con`, `last_ip`) VALUES
 (2, 'Compte', 'Admin', 'admin', '87c45b34c7447dd9ed4a0ea248e17653c5a3e59b', 'admin@admin.com', '2016-11-15 02:26:54', '::1');
 
 --
--- Index pour les tables exportées
+-- Indexes for dumped tables
 --
 
 --
--- Index pour la table `screen`
+-- Indexes for table `screen`
 --
 ALTER TABLE `screen`
   ADD PRIMARY KEY (`id`);
 
 --
--- Index pour la table `users`
+-- Indexes for table `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `username` (`username`);
 
 --
--- AUTO_INCREMENT pour les tables exportées
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT pour la table `screen`
+-- AUTO_INCREMENT for table `screen`
 --
 ALTER TABLE `screen`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 --
--- AUTO_INCREMENT pour la table `users`
+-- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
